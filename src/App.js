e3d19a406b35a453de88079fe1ab8ef352111aaf
xiaoguai88 <7438285+xiaoguai88@user.noreply.gitee.com>
2021-07-08 07:46:52 +0800

    fix:antd全局样式测试

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { Button } from "antd";
+import { HashRouter as Router, Route } from 'react-router-dom'
+import Home from './page/Home'
 import "./App.less";
 
 function App() {
-  return <Button>create-react-app</Button>;
+  return (<Router>
+    <Route path='/home' component={Home} exact />
+    create-react-app
+  </Router>)
 }
 
 export default App;
--- src/page/Home/index.jsx
+++ src/page/Home/index.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import { Button } from 'antd'
 
 class Home extends Component {
     render() {
         return (
             <div>
-                home
+                home<Button>home</Button>
             </div>
         )
     }
